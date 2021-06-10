ab2a6cc82d77a6a84c8ba43ea9ea013fe703c360
rdedo099 <43250257+rdedo099@users.noreply.github.com>
2021-06-10 01:38:21 -0400

    adjustments to test class causes success

--- Lab04/tic/test/TicTest.java
+++ Lab04/tic/test/TicTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class TicTest{
 
  @Test
- void test_updateBoard(){
+ public void test_updateBoard(){
    String[][] expected ={{"X","|_","|_"},{"_|","|_","|_"},{"_|","|_","|_"}};
    String[][] actual = Tic.updateBoard(0,0,1);
   assertArrayEquals(expected,actual);
