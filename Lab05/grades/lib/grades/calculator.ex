93f20180ac919f33a557a7546ce345bd53496dcd
rdedo099 <43250257+rdedo099@users.noreply.github.com>
2021-06-29 14:52:38 -0400

    Added Code From Assignment 2

---
+++ Lab05/grades/lib/grades/calculator.ex
@@ -0,0 +1,89 @@
+defmodule Grades.Calculator do
+def avg ([]) do
+0
+end
+def avg (x) do
+		Enum.sum(x) / Enum.count(x)
+	end
+def failed_to_participate(x, y, z) do
+x<0.4 || y <0.4 || z < 3
+end
+def calculate_grade(x, y, z, d) do
+0.2 * x + 0.3 * y + 0.2 * z + 0.3 * d
+end
+def provide_grading_letter(mark) do
+cond do
+        mark > 0.895 -> "A+"
+        mark > 0.845 -> "A"
+        mark > 0.795 -> "A-"
+        mark > 0.745 -> "B+"
+        mark > 0.695 -> "B"
+        mark > 0.645 -> "C+"
+        mark > 0.595 -> "C"
+        mark > 0.545 -> "D+"
+        mark > 0.495 -> "D"
+        mark > 0.395 -> "E"
+        :else -> "F"
+      end
+end
+def provide_grading_number(mark) do
+cond do
+        mark > 0.895 -> 10
+        mark > 0.845 -> 9
+        mark > 0.795 -> 8
+        mark > 0.745 -> 7
+        mark > 0.695 -> 6
+        mark > 0.645 -> 5
+        mark > 0.595 -> 4
+        mark > 0.545 -> 3
+        mark > 0.495 -> 2
+        mark > 0.395 -> 1
+        :else -> 0
+      end
+end
+  def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+    avg_homework = avg(homework)
+
+    avg_labs = avg(labs)
+    mark = calculate_grade(avg_labs, avg_homework, midterm, final)
+    round(mark * 100)
+  end
+
+  def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+    avg_homework = avg(homework)
+    avg_labs = avg(labs)
+    avg_exams = (midterm + final) / 2
+
+    num_labs =
+      labs
+      |> Enum.reject(fn mark -> mark < 0.25 end)
+      |> Enum.count()
+
+    if failed_to_participate(avg_homework, avg_exams, num_labs)do
+      "EIN"
+    else
+      mark = calculate_grade(avg_labs, avg_homework, midterm, final)
+
+      provide_grading_letter(mark)
+    end
+  end
+
+  def numeric_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+    avg_homework = avg(homework)
+    avg_labs = avg(labs)
+    avg_exams = (midterm + final) / 2
+
+    num_labs =
+      labs
+      |> Enum.reject(fn mark -> mark < 0.25 end)
+      |> Enum.count()
+
+    if failed_to_participate(avg_homework, avg_exams, num_labs)do
+      0
+    else
+      mark = calculate_grade(avg_labs, avg_homework, midterm, final)
+
+provide_grading_number(mark)
+    end
+  end
+end
